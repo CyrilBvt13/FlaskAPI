2365ca70f40ce19ee5efab656ea5736f1de60c47
Cyril Bouvart <75614845+CyrilBvt13@users.noreply.github.com>
2021-05-30 15:15:59 +0200

    Delete FlaskAPI directory

--- FlaskAPI/resources/routes.py
+++
@@ -1,6 +0,0 @@
-
-from .sensorDatas import SensorDataAPI, SensorDatasAPI
-
-def initialize_routes(api):
-    api.add_resource(SensorDatasAPI, "/flaskAPI/sensordata/")
-    api.add_resource(SensorDataAPI, "/flaskAPI/sensordata/<uid>")
